c7c53125502bf99f82f6617193427e15f205a251
Loy Yee Ko <koloyyee@gmail.com>
2023-02-09 23:03:08 +0800

    style(commit): setup commitlint & husky.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,26 +2,13 @@
 
 const Configuration = {
     extends: ['@commitlint/config-conventional'],
-    parserPreset: 'conventional-changelog-atom',
     formatter: '@commitlint/format',
     rules: {
         'type-enum': [
             2,
             'always',
-            ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']
-        ]
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert'],
+        ],
     },
-    ignores: [(commit) => commit === ''],
-    defaultIgnores: true,
-    helpUrl:
-        'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
-    prompt: {
-        messages: {},
-        questions: {
-            type: {
-                description: 'please input type:'
-            }
-        }
-    }
 };
 module.exports = Configuration;
